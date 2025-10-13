0daf6b644baab16474513d0af50c2330a588d13a
Kevin Nador <kevinnador19@gmail.com>
2025-10-12 23:09:10 -0300

    prueba

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 const Header = () => {
   return (
     <header style={{ backgroundColor: '#282c34', padding: '20px', color: 'white' }}>
-      <h1>Bienvenidos a mi app en React</h1>
+      <h1></h1>
     </header>
   );
 };
