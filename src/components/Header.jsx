cce8375bdcd9f329a72136ba5be0d2240a73027b
Kevin Nador <kevinnador19@gmail.com>
2025-10-18 11:19:53 -0300

    cambios

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,29 +1,11 @@
-import { Link } from "react-router-dom";
+
+import "./HeaderEstilo.css";
 
 const Header = () => {
   return (
     <header className="bg-gray-900 text-white p-6 flex items-center justify-between shadow-md">
-      <h1 className="text-2xl font-bold tracking-wide">Mi Tienda React</h1>
-      <nav>
-        <ul className="flex gap-6">
-          <li>
-            <Link
-              to="/inicio"
-              className="hover:text-gray-300 transition-colors"
-            >
-              Inicio
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="/contacto"
-              className="hover:text-gray-300 transition-colors"
-            >
-              Contacto
-            </Link>
-          </li>
-        </ul>
-      </nav>
+      <h1 className="titulo-header text-2xl font-bold tracking-wide">Olivia Aceituno</h1>
+        
     </header>
   );
 };
